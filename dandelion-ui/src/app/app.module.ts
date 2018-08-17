238e34683de0ecd2ad68c1cc182e8acb6b5c0101
Badarinarayan Burli <bburli@vmware.com>
2018-08-18 00:08:32 +0530

    Added clarity

--- dandelion-ui/src/app/app.module.ts
+++ dandelion-ui/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
